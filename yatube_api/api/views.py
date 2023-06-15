366e7ce307fdd8df66880564cefe33df355d3055
Vlad <stas.basov@whatever.com>
2023-06-15 19:16:58 +0300

    fixes

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -32,15 +32,14 @@ class CommentViewSet(ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (IsAuthenticatedOrReadOnly, OwnerOrReadOnly,)
 
+    def get_post(self):
+        return get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+
     def get_queryset(self):
-        post_id = self.kwargs.get('post_id')
-        post = get_object_or_404(Post, id=post_id)
-        return post.comments
+        return self.get_post().comments.all()
 
     def perform_create(self, serializer):
-        post_id = self.kwargs.get('post_id')
-        instance = get_object_or_404(Post, id=post_id)
-        serializer.save(author=self.request.user, post=instance)
+        serializer.save(author=self.request.user, post=self.get_post())
 
 
 class FollowViewSet(mixins.CreateModelMixin,
